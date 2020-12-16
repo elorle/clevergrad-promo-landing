42fed79a9015adf39e9e7de4df709a936135d9d6
elorle <m@elorle.ru>
2020-12-16 17:26:58 +0300

    kjdfhjkhgfd

--- script.js
+++ script.js
@@ -48,6 +48,12 @@ function ready() {
 		quizOptions[i].onclick = quizOptionClick.bind(quizOptions[i].getAttribute('for'),answers);
 	}
 
+	// установка маски при вводе телефона
+	var phoneMask = IMask(
+		document.getElementById('quizPhone'), {
+			mask: '+{7} (000) 000-00-00'
+	});
+
 	setTimeout(arrowOut, 700, elemIdArr, elemTagInContentArr, range, mainButton);
 
 	setTimeout(arrowHideAfterFirstTouch, 700, firstTouch, elemIdArr, elemTagInContentArr);
@@ -338,4 +344,47 @@ function arrowHide(elemIdArr,elemTagInContentArr) {
 
 function quizOptionClick(answers) {
 	answers[document.getElementById(this).name] = document.getElementById(this).value;
+}
+
+function mask(inputName, mask, evt) {
+	try {
+		var text = document.getElementById(inputName);
+		var value = text.value;
+
+		// If user pressed DEL or BACK SPACE, clean the value
+		try {
+			var e = (evt.which) ? evt.which : event.keyCode;
+			if ( e == 46 || e == 8 ) {
+				text.value = "";
+				return;
+			}
+		} catch (e1) {}
+
+		var literalPattern=/[0\*]/;
+		var numberPattern=/[0-9]/;
+		var newValue = "";
+
+		for (var vId = 0, mId = 0 ; mId < mask.length ; ) {
+			if (mId >= value.length)
+				break;
+
+			// Number expected but got a different value, store only the valid portion
+			if (mask[mId] == '0' && value[vId].match(numberPattern) == null) {
+				break;
+			}
+
+			// Found a literal
+			while (mask[mId].match(literalPattern) == null) {
+				if (value[vId] == mask[mId])
+					break;
+
+				newValue += mask[mId++];
+			}
+
+			newValue += value[vId++];
+			mId++;
+		}
+
+		text.value = newValue;
+	} catch(e) {}
 }
